e13231bb31b940ef9fc8a43a125b14812c158f80
Talha <70065773+infernotlc@users.noreply.github.com>
2024-07-28 15:48:12 +0300

    preview added

--- feature/src/main/java/com/yusuf/feature/home/ChooseSportScreen.kt
+++ feature/src/main/java/com/yusuf/feature/home/ChooseSportScreen.kt
@@ -3,21 +3,20 @@ package com.yusuf.feature.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.yusuf.feature.R
 
 @Composable
-fun HomeScreen(navController: NavController) {
+fun ChooseSportScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -32,4 +31,10 @@ fun HomeScreen(navController: NavController) {
             Text(text = "Go to Other Page")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChooseSportScreenPreview(){
+    ChooseSportScreen(navController = rememberNavController())
 }
