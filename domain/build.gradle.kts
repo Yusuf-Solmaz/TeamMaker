991873a5c7f90af2df3553db276796f419075a92
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-07-29 16:43:59 +0300

    feat: added weather use case and view model

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.hilt.android)
 }
