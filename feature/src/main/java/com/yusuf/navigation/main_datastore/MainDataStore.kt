48e7d3cb8be0789eb408654cf7ab48b27ead0bac
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-29 13:03:56 +0300

    feat: completed MainDataStore

---
+++ feature/src/main/java/com/yusuf/navigation/main_datastore/MainDataStore.kt
@@ -0,0 +1,47 @@
+package com.yusuf.navigation.main_datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+val Context.myPreferencesDataStore: DataStore<Preferences> by preferencesDataStore( "settings")
+
+@Singleton
+class MainDataStore @Inject constructor(
+    @ApplicationContext context: Context
+) {
+    private val myPreferencesDataStore = context.myPreferencesDataStore
+
+    private object PreferencesKeys{
+        val APP_ENTRY_KEY = booleanPreferencesKey("app_entry")
+    }
+
+    val readAppEntry = myPreferencesDataStore.data.catch {
+            exception->
+        if (exception is IOException){
+            emit(emptyPreferences())
+        }else{
+            throw exception
+        }
+    }.map {
+            preferences ->
+        preferences[PreferencesKeys.APP_ENTRY_KEY] ?: true
+    }
+
+    suspend fun saveAppEntry(){
+        myPreferencesDataStore.edit {
+                preferences ->
+            preferences[PreferencesKeys.APP_ENTRY_KEY] = false
+        }
+    }
+}
