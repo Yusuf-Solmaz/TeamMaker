fee882d667f4e4b4180b50835fc4001f0ee33e69
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-29 14:44:31 +0300

    feat: added firebase mapper

---
+++ data/src/main/java/com/yusuf/data/mapper/FirebasePlayerMapper.kt
@@ -0,0 +1,14 @@
+package com.yusuf.data.mapper
+
+import com.yusuf.data.remote.dto.firebase_dto.PlayerDataDto
+import com.yusuf.domain.model.firebase.PlayerData
+
+fun PlayerDataDto.toPlayerData(): PlayerData {
+    return PlayerData(
+        profilePhotoUrl = profilePhotoUrl,
+        firstName = firstName,
+        lastName = lastName,
+        position = position,
+        skillRating = skillRating
+    )
+}
