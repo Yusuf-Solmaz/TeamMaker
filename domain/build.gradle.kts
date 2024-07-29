c81dcc2e1266b7728f5ff1d62c185d3cc2124b8b
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-29 14:43:15 +0300

    docs: added firebase dependencies

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -40,4 +41,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
 }
