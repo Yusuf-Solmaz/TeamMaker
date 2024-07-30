596b248934c1ac3b4a8bef6a4a35b0724f209616
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-30 12:48:28 +0300

    feat: added competition models

---
+++ data/src/main/java/com/yusuf/data/remote/dto/firebase_dto/CompetitionDataDto.kt
@@ -0,0 +1,7 @@
+package com.yusuf.data.remote.dto.firebase_dto
+
+data class CompetitionDataDto(
+    val competitionName: String,
+    val competitionDescription: String,
+    val competitionImageUrl: String
+)
