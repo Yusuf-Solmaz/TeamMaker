116199008a2ab9a8479d86ebf4baef99bc1c2608
Talha <70065773+infernotlc@users.noreply.github.com>
2024-07-30 20:12:17 +0300

    feat:add, delete, get all implementations

--- data/src/main/java/com/yusuf/data/repository/firebase/PlayerRepositoryImpl.kt
+++ data/src/main/java/com/yusuf/data/repository/firebase/PlayerRepositoryImpl.kt
@@ -14,7 +14,6 @@ import com.yusuf.domain.repository.firebase.player.PlayerRepository
 import com.yusuf.domain.util.RootResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.tasks.await
@@ -23,32 +22,27 @@ import javax.inject.Inject
 class PlayerRepositoryImpl @Inject constructor(
     private val firebaseAuth: FirebaseAuth,
     private val firestore: FirebaseFirestore
-): PlayerRepository{
+): PlayerRepository {
 
     override suspend fun getAllPlayers(): Flow<RootResult<List<PlayerData>>> = flow {
         emit(RootResult.Loading)
         try {
-            val userIdResult = getCurrentUserId().first()
-            if (userIdResult is RootResult.Success) {
-                val userId = userIdResult.data
-                if (userId != null) {
-                    val querySnapshot = firestore.collection("users").document(userId).collection("players").get().await()
-                    val playerList = querySnapshot.documents.mapNotNull { document ->
-                        val playerDataDto = document.toObject(PlayerDataDto::class.java)
-                        playerDataDto?.toPlayerData()
-                    }
-                    emit(RootResult.Success(playerList))
-                } else {
-                    emit(RootResult.Error("User ID is null"))
+            val currentUser = firebaseAuth.currentUser
+            val userId = currentUser?.uid
+            if (userId != null) {
+                val querySnapshot = firestore.collection("users").document(userId).collection("players").get().await()
+                val playerList = querySnapshot.documents.mapNotNull { document ->
+                    val playerDataDto = document.toObject(PlayerDataDto::class.java)
+                    playerDataDto?.toPlayerData()
                 }
+                emit(RootResult.Success(playerList))
             } else {
-                emit(RootResult.Error("Failed to get user ID"))
+                emit(RootResult.Error("User ID is null"))
             }
         } catch (e: Exception) {
             emit(RootResult.Error(e.message ?: "Something went wrong"))
         }
     }.flowOn(Dispatchers.IO)
-
 
     override fun getCurrentUserId(): Flow<RootResult<String?>> = flow {
         emit(RootResult.Loading)
@@ -63,68 +57,83 @@ class PlayerRepositoryImpl @Inject constructor(
 
 
     override suspend fun addPlayer(playerData: PlayerData): Flow<RootResult<Boolean>> = flow {
-
-        val playerInfo = playerData.toPlayerDataDto()
-
         emit(RootResult.Loading)
         try {
-            val userIdResult = getCurrentUserId().first()
-            if (userIdResult is RootResult.Success) {
-                val userId = userIdResult.data
-                if (userId != null) {
-                    firestore.collection("users").document(userId).collection("players").add(playerInfo).await()
-                    emit(RootResult.Success(true))
-                } else {
-                    emit(RootResult.Error("User ID is null"))
-                }
+            val currentUser = firebaseAuth.currentUser
+            val userId = currentUser?.uid
+            if (userId != null) {
+                val playerInfo = playerData.toPlayerDataDto()
+                firestore.collection("users").document(userId).collection("players").add(playerInfo).await()
+                emit(RootResult.Success(true))
             } else {
-                emit(RootResult.Error("Failed to get user ID"))
+                emit(RootResult.Error("User ID is null"))
             }
         } catch (e: Exception) {
             emit(RootResult.Error(e.message ?: "Something went wrong"))
         }
     }.flowOn(Dispatchers.IO)
+
 
     override suspend fun addCompetition(competitionData: CompetitionData): Flow<RootResult<Boolean>> = flow {
-        val competitionInfo = competitionData.toCompetitionDataDto()
-
         emit(RootResult.Loading)
         try {
-            val userIdResult = getCurrentUserId().first()
-            if (userIdResult is RootResult.Success) {
-                val userId = userIdResult.data
-                if (userId != null) {
-                    firestore.collection("users").document(userId).collection("competitions").add(competitionInfo).await()
-                    emit(RootResult.Success(true))
-                } else {
-                    emit(RootResult.Error("User ID is null"))
-                }
+            val currentUser = firebaseAuth.currentUser
+            val userId = currentUser?.uid
+            if (userId != null) {
+                val competitionInfo = competitionData.toCompetitionDataDto()
+                firestore.collection("users").document(userId).collection("competitions").add(competitionInfo).await()
+                emit(RootResult.Success(true))
             } else {
-                emit(RootResult.Error("Failed to get user ID"))
+                emit(RootResult.Error("User ID is null"))
             }
         } catch (e: Exception) {
             emit(RootResult.Error(e.message ?: "Something went wrong"))
         }
     }.flowOn(Dispatchers.IO)
+
+    override suspend fun deleteCompetition(competitionId: String): Flow<RootResult<Boolean>> = flow {
+        emit(RootResult.Loading)
+        try {
+            val currentUser = firebaseAuth.currentUser
+            val userId = currentUser?.uid
+            if (userId != null) {
+                val competitionCollection = firestore.collection("users").document(userId).collection("competitions")
+                val querySnapshot = competitionCollection.whereEqualTo("competitionId", competitionId).get().await()
+                val competitionDocuments = querySnapshot.documents
+
+                if (competitionDocuments.isNotEmpty()) {
+                    competitionDocuments.forEach { document ->
+                        document.reference.delete().await()
+                    }
+                    emit(RootResult.Success(true))
+                } else {
+                    emit(RootResult.Error("Player not found"))
+                }
+            } else {
+                emit(RootResult.Error("User ID is null"))
+            }
+        } catch (e: Exception) {
+            emit(RootResult.Error(e.message ?: "Something went wrong"))
+        }
+    }.flowOn(Dispatchers.IO)
+
+
+
 
     override suspend fun getAllCompetitions(): Flow<RootResult<List<CompetitionData>>> = flow {
         emit(RootResult.Loading)
         try {
-            val userIdResult = getCurrentUserId().first()
-            if (userIdResult is RootResult.Success) {
-                val userId = userIdResult.data
-                if (userId != null) {
-                    val querySnapshot = firestore.collection("users").document(userId).collection("competitions").get().await()
-                    val competitionList = querySnapshot.documents.mapNotNull { document ->
-                        val competitionDataDto = document.toObject(CompetitionDataDto::class.java)
-                        competitionDataDto?.toCompetitionData()
-                    }
-                    emit(RootResult.Success(competitionList))
-                } else {
-                    emit(RootResult.Error("User ID is null"))
+            val currentUser = firebaseAuth.currentUser
+            val userId = currentUser?.uid
+            if (userId != null) {
+                val querySnapshot = firestore.collection("users").document(userId).collection("competitions").get().await()
+                val competitionList = querySnapshot.documents.mapNotNull { document ->
+                    val competitionDataDto = document.toObject(CompetitionDataDto::class.java)
+                    competitionDataDto?.toCompetitionData()
                 }
+                emit(RootResult.Success(competitionList))
             } else {
-                emit(RootResult.Error("Failed to get user ID"))
+                emit(RootResult.Error("User ID is null"))
             }
         } catch (e: Exception) {
             emit(RootResult.Error(e.message ?: "Something went wrong"))
