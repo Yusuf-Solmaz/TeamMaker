390b727ccdbad31029d79d0e0bfbe434512bc374
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-07-30 13:54:09 +0300

    feat: added gelLocationUseCase and appModule

---
+++ data/src/main/java/com/yusuf/data/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.yusuf.data.di
+
+import android.content.Context
+import com.yusuf.data.repository.LocationRepositoryImpl
+import com.yusuf.domain.repository.LocationRepository
+import com.yusuf.domain.use_cases.GetLocationUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocationRepository(
+        context: Context
+    ): LocationRepository {
+        return LocationRepositoryImpl(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocationUseCase(
+        locationRepository: LocationRepository
+    ): GetLocationUseCase {
+        return GetLocationUseCase(locationRepository)
+    }
+}
