28b06925050ac039e5363be2beefe39b3a950cf8
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-30 21:30:52 +0300

    feat: added player id

--- data/src/main/java/com/yusuf/data/remote/dto/firebase_dto/PlayerDataDto.kt
+++ data/src/main/java/com/yusuf/data/remote/dto/firebase_dto/PlayerDataDto.kt
@@ -1,10 +1,14 @@
 package com.yusuf.data.remote.dto.firebase_dto
 
+import java.io.Serializable
+import java.util.UUID
+
 data class PlayerDataDto(
-    val profilePhotoUrl: String,
-    val firstName: String,
-    val lastName: String,
-    val position: String,
-    val skillRating: Int
-)
+    val id: String = UUID.randomUUID().toString(),
+    val profilePhotoUrl: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val position: String = "",
+    val skillRating: Int = 0
+): Serializable
 
