31f0258090790e1dcc3d186097a476a734edceac
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-31 19:06:55 +0300

    feat: added main content screen

--- app/src/main/java/com/yusuf/teammaker/MainActivity.kt
+++ app/src/main/java/com/yusuf/teammaker/MainActivity.kt
@@ -5,19 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.yusuf.navigation.TeamMakerNavigation
-import com.yusuf.navigation.main_viewmodel.MainViewModel
-import com.yusuf.teammaker.ui.theme.TeamMakerTheme
+import androidx.navigation.compose.rememberNavController
+import com.yusuf.feature.main_content.MainScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val mainViewModel by viewModels<MainViewModel>()
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,13 +18,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TeamMakerTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    content = {
-                        TeamMakerNavigation(mainViewModel = mainViewModel)
-                    })
+            val navController = rememberNavController()
+            MainScreen(navController = navController)
             }
         }
-    }
+
 }
+
