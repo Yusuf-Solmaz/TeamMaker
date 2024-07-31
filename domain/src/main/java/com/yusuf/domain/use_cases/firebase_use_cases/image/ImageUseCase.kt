6ee54d820df5410e423d7fef26f557b53bc0b14b
Talha <70065773+infernotlc@users.noreply.github.com>
2024-07-31 18:55:59 +0300

    feat:changed to add storage, retrieving url and show with coil

--- domain/src/main/java/com/yusuf/domain/use_cases/firebase_use_cases/image/ImageUseCase.kt
+++ domain/src/main/java/com/yusuf/domain/use_cases/firebase_use_cases/image/ImageUseCase.kt
@@ -1,5 +1,6 @@
 package com.yusuf.domain.use_cases.firebase_use_cases.image
 
+import android.graphics.Bitmap
 import android.net.Uri
 import com.yusuf.domain.repository.firebase.image.ImageRepository
 import javax.inject.Inject
@@ -7,7 +8,7 @@ import javax.inject.Inject
 class ImageUseCase @Inject constructor(
     private val repository: ImageRepository
 ) {
-    suspend operator fun invoke(uri: Uri): Result<String> {
-        return repository.uploadImage(uri)
+    suspend operator fun invoke(bitmap: Bitmap, competitionName: String): Result<String> {
+        return repository.uploadImageToStorage(bitmap, competitionName)
     }
 }
