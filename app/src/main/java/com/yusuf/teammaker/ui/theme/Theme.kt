9f840d3e90a350d2162573f549f636602eb942fd
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-31 19:08:59 +0300

    feat: update dark theme

--- app/src/main/java/com/yusuf/teammaker/ui/theme/Theme.kt
+++ app/src/main/java/com/yusuf/teammaker/ui/theme/Theme.kt
@@ -35,7 +35,8 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun TeamMakerTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    //darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
