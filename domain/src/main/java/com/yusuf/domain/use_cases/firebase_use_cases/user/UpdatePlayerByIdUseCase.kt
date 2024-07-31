10e07089d6e30f8bb1014863d09a2034e46d3b17
Talha <70065773+infernotlc@users.noreply.github.com>
2024-07-31 20:15:26 +0300

    refactor:new parameters added

--- domain/src/main/java/com/yusuf/domain/use_cases/firebase_use_cases/user/UpdatePlayerByIdUseCase.kt
+++ domain/src/main/java/com/yusuf/domain/use_cases/firebase_use_cases/user/UpdatePlayerByIdUseCase.kt
@@ -1,5 +1,6 @@
 package com.yusuf.domain.use_cases.firebase_use_cases.user
 
+import android.net.Uri
 import com.yusuf.domain.model.firebase.PlayerData
 import com.yusuf.domain.repository.firebase.player.PlayerRepository
 import javax.inject.Inject
@@ -7,6 +8,6 @@ import javax.inject.Inject
 class UpdatePlayerByIdUseCase @Inject constructor(
     private val playerRepository: PlayerRepository
 ) {
-    suspend operator fun invoke(playerId: String, updatedPlayerData: PlayerData) =
-        playerRepository.updatePlayerById(playerId, updatedPlayerData)
+    suspend operator fun invoke(playerId: String, updatedPlayerData: PlayerData,imageUri:Uri) =
+        playerRepository.updatePlayerById(playerId, updatedPlayerData,imageUri)
 }
