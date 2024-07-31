cd23384927d40fd235ec86c73f3dea0684fdf583
Talha <70065773+infernotlc@users.noreply.github.com>
2024-07-31 18:54:47 +0300

    refactor:changed to add storage, retrieving url and show with coil

--- domain/src/main/java/com/yusuf/domain/repository/firebase/image/ImageRepository.kt
+++ domain/src/main/java/com/yusuf/domain/repository/firebase/image/ImageRepository.kt
@@ -1,7 +1,7 @@
 package com.yusuf.domain.repository.firebase.image
 
 import android.graphics.Bitmap
-import android.net.Uri
+
 
     interface ImageRepository {
         suspend fun uploadImageToStorage(bitmap: Bitmap, competitionName: String): Result<String>
