246603a8fae04dec74e7cc3cf7867cfd6a4cda30
Talha <70065773+infernotlc@users.noreply.github.com>
2024-07-31 18:55:41 +0300

    feat:changed to add storage, retrieving url and show with coil

--- data/src/main/java/com/yusuf/data/repository/firebase/ImageRepositoryImpl.kt
+++ data/src/main/java/com/yusuf/data/repository/firebase/ImageRepositoryImpl.kt
@@ -1,19 +1,31 @@
 package com.yusuf.data.repository.firebase
 
+import android.graphics.Bitmap
 import android.net.Uri
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.yusuf.domain.repository.firebase.image.ImageRepository
 import kotlinx.coroutines.tasks.await
+import java.io.ByteArrayOutputStream
+import java.util.UUID
 import javax.inject.Inject
 
 class ImageRepositoryImpl @Inject constructor(
-    private val storage: FirebaseStorage
+    private val storage: FirebaseStorage,
+    private val firestore: FirebaseFirestore
 ) : ImageRepository {
 
-    override suspend fun uploadImage(uri: Uri): Result<String> {
+    override suspend fun uploadImageToStorage(
+        bitmap: Bitmap,
+        competitionName: String
+    ): Result<String> {
         return try {
-            val storageRef = storage.reference.child("images/${uri.lastPathSegment}")
-            storageRef.putFile(uri).await()
+            val storageRef = storage.reference.child("competitions/${UUID.randomUUID()}.jpg")
+            val baos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
+
+            storageRef.putBytes(data).await()
             val downloadUrl = storageRef.downloadUrl.await().toString()
             Result.success(downloadUrl)
         } catch (e: Exception) {
