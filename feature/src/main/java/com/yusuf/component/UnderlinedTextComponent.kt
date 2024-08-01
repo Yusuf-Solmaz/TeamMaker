69d8e9fc50d00ec8607bb95c3c0728efd81b093f
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-01 18:43:15 +0300

    style:redundant dependencies removed

--- feature/src/main/java/com/yusuf/component/UnderlinedTextComponent.kt
+++ feature/src/main/java/com/yusuf/component/UnderlinedTextComponent.kt
@@ -2,7 +2,6 @@ package com.yusuf.component
 
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
