9009ad9b532d898e4d2b24aa78eb794de48d8058
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-04 02:03:43 +0300

    feat:read and save tip states in datastore

--- feature/src/main/java/com/yusuf/navigation/main_datastore/MainDataStore.kt
+++ feature/src/main/java/com/yusuf/navigation/main_datastore/MainDataStore.kt
@@ -8,6 +8,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
@@ -22,26 +23,44 @@ class MainDataStore @Inject constructor(
 ) {
     private val myPreferencesDataStore = context.myPreferencesDataStore
 
-    private object PreferencesKeys{
+    private object PreferencesKeys {
         val APP_ENTRY_KEY = booleanPreferencesKey("app_entry")
+        val TOOLTIP_SHOWN_KEY = booleanPreferencesKey("tooltip_shown")
     }
 
-    val readAppEntry = myPreferencesDataStore.data.catch {
-            exception->
-        if (exception is IOException){
-            emit(emptyPreferences())
-        }else{
-            throw exception
+    val readAppEntry: Flow<Boolean> = myPreferencesDataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
         }
-    }.map {
-            preferences ->
-        preferences[PreferencesKeys.APP_ENTRY_KEY] ?: true
+        .map { preferences ->
+            preferences[PreferencesKeys.APP_ENTRY_KEY] ?: true
+        }
+
+    val readTooltipShown: Flow<Boolean> = myPreferencesDataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+        .map { preferences ->
+            preferences[PreferencesKeys.TOOLTIP_SHOWN_KEY] ?: false
+        }
+
+    suspend fun saveAppEntry() {
+        myPreferencesDataStore.edit { preferences ->
+            preferences[PreferencesKeys.APP_ENTRY_KEY] = false
+        }
     }
 
-    suspend fun saveAppEntry(){
-        myPreferencesDataStore.edit {
-                preferences ->
-            preferences[PreferencesKeys.APP_ENTRY_KEY] = false
+    suspend fun saveTooltipShown() {
+        myPreferencesDataStore.edit { preferences ->
+            preferences[PreferencesKeys.TOOLTIP_SHOWN_KEY] = true
         }
     }
 }
