f06b3cd454f9b4d43f589afed4edc7d8b7d6d7cc
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-08-04 13:57:01 +0300

    feat: add general skill point

--- feature/src/main/java/com/yusuf/feature/player_list/PlayerListScreen.kt
+++ feature/src/main/java/com/yusuf/feature/player_list/PlayerListScreen.kt
@@ -41,10 +41,13 @@ import androidx.wear.compose.material.FractionalThreshold
 import androidx.wear.compose.material.rememberSwipeableState
 import androidx.wear.compose.material.swipeable
 import coil.compose.rememberAsyncImagePainter
+import com.yusuf.component.DividerTextComponent
 import com.yusuf.component.LoadingLottie
 import com.yusuf.component.TextFieldComponent
 import com.yusuf.domain.model.firebase.PlayerData
 import com.yusuf.feature.R
+import com.yusuf.feature.player_list.custom_dialog.AddPlayerDialog
+import com.yusuf.feature.player_list.custom_dialog.UpdatePlayerDialog
 import com.yusuf.feature.player_list.viewmodel.PlayerListViewModel
 import com.yusuf.utils.SharedPreferencesHelper
 import kotlin.math.roundToInt
@@ -55,6 +58,7 @@ fun PlayerListScreen(
     viewModel: PlayerListViewModel = hiltViewModel()
 ) {
     val playerListUiState by viewModel.playerListUIState.collectAsState()
+    val playerTransactionUiState by viewModel.playerUiState.collectAsState()
 
     var showAddPlayerDialog by remember { mutableStateOf(false) }
     var showUpdatePlayerDialog by remember { mutableStateOf(false) }
@@ -82,7 +86,8 @@ fun PlayerListScreen(
                     .fillMaxSize()
             ) {
                 when {
-                    playerListUiState.isLoading -> {
+
+                    playerListUiState.isLoading or playerTransactionUiState.isLoading -> {
                         Box(
                             modifier = Modifier
                                 .fillMaxSize(),
@@ -171,15 +176,17 @@ fun PlayerListScreen(
                                 focus = playerData.focus,
                                 condition = playerData.condition,
                                 durability = playerData.durability,
+                                generalSkill = playerData.generalSkill,
                                 totalSkillRating = playerData.totalSkillRating
                             ),
                             Uri.parse(playerData.profilePhotoUrl)
                         )
-                        viewModel.getPlayersByCompetitionType(competitionName.toString()) // Refresh the list
+                        viewModel.getPlayersByCompetitionType(competitionName.toString())
                         showAddPlayerDialog = false
                     },
                     updateList = {
-                    }
+                    },
+                    context = context
                 )
             }
 
@@ -245,7 +252,6 @@ fun PlayerListItem(
             }
         }
 
-        // Background for Delete button
         Box(
             modifier = Modifier
                 .fillMaxHeight()
@@ -331,329 +337,4 @@ fun PlayerListItem(
 
 
 
-@Composable
-fun AddPlayerDialog(
-    competitionName: String,
-    onDismiss: () -> Unit,
-    onAddPlayer: (playerData: PlayerData) -> Unit,
-    updateList: () -> Unit
-) {
-    var profilePhotoUri by remember { mutableStateOf<Uri?>(null) }
-    var firstName by remember { mutableStateOf("") }
-    var lastName by remember { mutableStateOf("") }
-    var position by remember { mutableStateOf("") }
-    var speed by remember { mutableStateOf(0) }
-    var focus by remember { mutableStateOf(0) }
-    var condition by remember { mutableStateOf(0) }
-    var durability by remember { mutableStateOf(0) }
 
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent()
-    ) { uri: Uri? ->
-        profilePhotoUri = uri
-    }
-
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = { Text("Add Player") },
-        text = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.surface)
-                        .clickable { launcher.launch("image/*") }
-                        .align(Alignment.CenterHorizontally)
-                ) {
-                    if (profilePhotoUri != null) {
-                        Image(
-                            painter = rememberAsyncImagePainter(profilePhotoUri),
-                            contentDescription = "Profile Photo",
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .clip(CircleShape),
-                            contentScale = ContentScale.Fit
-                        )
-                    } else {
-                        Icon(
-                            imageVector = Icons.Filled.Add,
-                            contentDescription = "Select Photo",
-                            modifier = Modifier
-                                .align(Alignment.Center)
-                                .size(50.dp),
-                            tint = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                LazyColumn {
-                    item {
-                        TextFieldComponent(
-                            stateValue = firstName,
-                            label = "First Name",
-                            onValueChange = { firstName = it },
-                            painterResource = painterResource(id = R.drawable.ic_person)
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        TextFieldComponent(
-                            stateValue = lastName,
-                            label = "Last Name",
-                            onValueChange = { lastName = it },
-                            painterResource = painterResource(id = R.drawable.ic_person)
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        TextFieldComponent(
-                            stateValue = position,
-                            label = "Position",
-                            onValueChange = { position = it },
-                            painterResource = painterResource(id = R.drawable.ic_position)
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text("Speed: $speed", fontSize = 16.sp)
-                        Slider(
-                            value = speed.toFloat(),
-                            onValueChange = { speed = it.toInt() },
-                            valueRange = 0f..10f,
-                            steps = 10,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text("Condition: $condition", fontSize = 16.sp)
-                        Slider(
-                            value = condition.toFloat(),
-                            onValueChange = { condition = it.toInt() },
-                            valueRange = 0f..10f,
-                            steps = 10,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text("Focus: $focus", fontSize = 16.sp)
-                        Slider(
-                            value = focus.toFloat(),
-                            onValueChange = { focus = it.toInt() },
-                            valueRange = 0f..10f,
-                            steps = 10,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text("Durability: $durability", fontSize = 16.sp)
-                        Slider(
-                            value = durability.toFloat(),
-                            onValueChange = { durability = it.toInt() },
-                            valueRange = 0f..10f,
-                            steps = 10,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
-            }
-        },
-        confirmButton = {
-            Button(onClick = {
-                onAddPlayer(
-                    PlayerData(
-                        profilePhotoUrl = profilePhotoUri.toString(),
-                        firstName = firstName,
-                        lastName = lastName,
-                        position = position,
-                        competitionType = competitionName,
-                        speed = speed,
-                        focus = focus,
-                        condition = condition,
-                        durability = durability,
-                        totalSkillRating = speed + focus + condition + durability
-                    )
-                )
-
-                updateList()
-            }) {
-                Text("Add")
-            }
-        },
-        dismissButton = {
-            Button(onClick = onDismiss) {
-                Text("Cancel")
-            }
-        }
-    )
-}
-
-
-@Composable
-fun UpdatePlayerDialog(
-    playerData: PlayerData,
-    onDismiss: () -> Unit,
-    onUpdatePlayer: (PlayerData) -> Unit,
-    viewModel: PlayerListViewModel
-) {
-    var profilePhotoUri by remember { mutableStateOf<Uri?>(playerData.profilePhotoUrl.toUri()) }
-    var firstName by remember { mutableStateOf(playerData.firstName) }
-    var lastName by remember { mutableStateOf(playerData.lastName) }
-    var position by remember { mutableStateOf(playerData.position) }
-    var speed by remember { mutableStateOf(playerData.speed) }
-    var focus by remember { mutableStateOf(playerData.focus) }
-    var condition by remember { mutableStateOf(playerData.condition) }
-    var durability by remember { mutableStateOf(playerData.durability) }
-
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent()
-    ) { uri: Uri? ->
-        if (uri != null) {
-            profilePhotoUri = uri
-        }
-    }
-
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = { Text("Update Player") },
-        text = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.surface)
-                        .clickable { launcher.launch("image/*") }
-                        .align(Alignment.CenterHorizontally)
-                ) {
-                    if (profilePhotoUri != null) {
-                        Image(
-                            painter = rememberAsyncImagePainter(profilePhotoUri),
-                            contentDescription = "Profile Photo",
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .clip(CircleShape),
-                            contentScale = ContentScale.Crop
-                        )
-                    } else {
-                        Icon(
-                            imageVector = Icons.Filled.Edit,
-                            contentDescription = "Select Photo",
-                            modifier = Modifier
-                                .align(Alignment.Center)
-                                .size(50.dp),
-                            tint = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                LazyColumn {
-                    item {
-                        TextFieldComponent(
-                            stateValue = firstName,
-                            label = "First Name",
-                            onValueChange = { firstName = it },
-                            painterResource = painterResource(id = R.drawable.ic_person)
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        TextFieldComponent(
-                            stateValue = lastName,
-                            label = "Last Name",
-                            onValueChange = { lastName = it },
-                            painterResource = painterResource(id = R.drawable.ic_person)
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        TextFieldComponent(
-                            stateValue = position,
-                            label = "Position",
-                            onValueChange = { position = it },
-                            painterResource = painterResource(id = R.drawable.ic_position)
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text("Speed: $speed", fontSize = 16.sp)
-                        Slider(
-                            value = speed.toFloat(),
-                            onValueChange = { speed = it.toInt() },
-                            valueRange = 0f..10f,
-                            steps = 10,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text("Condition: $condition", fontSize = 16.sp)
-                        Slider(
-                            value = condition.toFloat(),
-                            onValueChange = { condition = it.toInt() },
-                            valueRange = 0f..10f,
-                            steps = 10,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text("Focus: $focus", fontSize = 16.sp)
-                        Slider(
-                            value = focus.toFloat(),
-                            onValueChange = { focus = it.toInt() },
-                            valueRange = 0f..10f,
-                            steps = 10,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text("Durability: $durability", fontSize = 16.sp)
-                        Slider(
-                            value = durability.toFloat(),
-                            onValueChange = { durability = it.toInt() },
-                            valueRange = 0f..10f,
-                            steps = 10,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
-            }
-        },
-        confirmButton = {
-            Button(onClick = {
-                if (profilePhotoUri != playerData.profilePhotoUrl.toUri()) {
-                    profilePhotoUri?.let { uri ->
-                        viewModel.updatePlayerImage(
-                            uri = uri,
-                            onSuccess = { downloadUrl ->
-                                onUpdatePlayer(
-                                    playerData.copy(
-                                        firstName = firstName,
-                                        lastName = lastName,
-                                        position = position,
-                                        speed = speed,
-                                        focus = focus,
-                                        condition = condition,
-                                        durability = durability,
-                                        totalSkillRating = speed + focus + condition + durability,
-                                        profilePhotoUrl = downloadUrl
-                                    )
-                                )
-                            },
-                            onFailure = { /* Handle failure */ }
-                        )
-                    }
-                } else {
-                    onUpdatePlayer(
-                        playerData.copy(
-                            firstName = firstName,
-                            lastName = lastName,
-                            position = position,
-                            speed = speed,
-                            focus = focus,
-                            condition = condition,
-                            durability = durability,
-                            totalSkillRating = speed + focus + condition + durability
-                        )
-                    )
-                }
-            }) {
-                Text("Update")
-            }
-        },
-        dismissButton = {
-            Button(onClick = onDismiss) {
-                Text("Cancel")
-            }
-        }
-    )
-}
