61b0d648abebe85915d9e429d2bc2fa6f5acac35
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-08-04 13:07:11 +0300

    style: tabrow designed

--- feature/src/main/java/com/yusuf/feature/competition_detail/TeamListScreen.kt
+++ feature/src/main/java/com/yusuf/feature/competition_detail/TeamListScreen.kt
@@ -1,11 +1,14 @@
 package com.yusuf.feature.competition_detail
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -16,9 +19,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yusuf.domain.model.firebase.PlayerData
+import com.yusuf.theme.Green
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -32,52 +38,70 @@ fun TeamListScreen(
     )
     val coroutineScope = rememberCoroutineScope()
 
-    Column {
-        TabRow(
-            selectedTabIndex = pagerState.currentPage,
-            containerColor = Color.Transparent,
-            contentColor = MaterialTheme.colorScheme.primary,
-            divider = {},
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier
-                        .tabIndicatorOffset(tabPositions[pagerState.currentPage])
-                        .height(2.dp),
-                    color = Color.White
-                )
-            }
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
         ) {
-            Tab(
-                selected = pagerState.currentPage==0,
-                onClick = {
-                    coroutineScope.launch {
-                        pagerState.animateScrollToPage(0)
-                    }
-                },
-                text = {
-                    Text(
-                        text = "First Team",
-                        color = if (pagerState.currentPage == 0) MaterialTheme.colorScheme.primary else Color.Gray,
-                        fontWeight = if (pagerState.currentPage == 0) FontWeight.Bold else FontWeight.Normal)
-                }
-            )
-            Tab(
-                selected = pagerState.currentPage==1,
-                onClick = {
-                    coroutineScope.launch {
-                        pagerState.animateScrollToPage(1)
-                    }
-                },
-                text = {
-                    Text(
-                        text = "Second Team",
-                        color = if (pagerState.currentPage == 1) MaterialTheme.colorScheme.primary else Color.Gray,
-                        fontWeight = if (pagerState.currentPage == 1) FontWeight.Bold else FontWeight.Normal
+            TabRow(
+                selectedTabIndex = pagerState.currentPage,
+                containerColor = Color.Transparent,
+                contentColor = MaterialTheme.colorScheme.primary,
+                divider = {},
+                indicator = { tabPositions ->
+                    TabRowDefaults.Indicator(
+                        Modifier
+                            .tabIndicatorOffset(tabPositions[pagerState.currentPage])
+                            .height(2.dp),
+                        color = Color.White
                     )
                 }
-            )
-        }
-
+            ) {
+                Tab(
+                    selected = pagerState.currentPage == 0,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(0)
+                        }
+                    },
+                    modifier = Modifier
+                        .background(if (pagerState.currentPage == 0) Green else MaterialTheme.colorScheme.background)
+                        .padding(10.dp)
+                ) {
+                    Text(
+                        text = "First Team",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = if (pagerState.currentPage == 0) Color.White else Green
+                        )
+                    )
+                }
+                Tab(
+                    selected = pagerState.currentPage == 1,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(1)
+                        }
+                    },
+                    modifier = Modifier
+                        .background(if (pagerState.currentPage == 1) Green else MaterialTheme.colorScheme.background)
+                        .padding(10.dp)
+                    ) {
+                        Text(
+                            text = "Second Team",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = if (pagerState.currentPage == 1) Color.White else Green
+                            )
+                        )
+                    }
+                }
+            }
         HorizontalPager(state = pagerState)  { page ->
             when (page) {
                 0 -> TeamList(team = firstTeam ?: emptyList())
