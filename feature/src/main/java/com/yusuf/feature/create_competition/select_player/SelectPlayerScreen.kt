b8ccf1f80624e0416e83c81c014e64ce792cc2bd
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-05 21:28:59 +0300

    feat:flip effect added, details moved to backcard

--- feature/src/main/java/com/yusuf/feature/create_competition/select_player/SelectPlayerScreen.kt
+++ feature/src/main/java/com/yusuf/feature/create_competition/select_player/SelectPlayerScreen.kt
@@ -1,16 +1,21 @@
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -27,12 +32,15 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -64,7 +72,6 @@ fun SelectPlayerScreen(
     sharedViewModel: TeamBalancerViewModel = hiltViewModel(),
     timePicker: String = ""
 ) {
-
     val viewModel: SelectPlayerViewModel = hiltViewModel()
     val playerListUiState by viewModel.playerListUIState.collectAsState()
     val teamBalancerUIState by sharedViewModel.teamBalancerUiState.collectAsState()
@@ -73,7 +80,7 @@ fun SelectPlayerScreen(
     val sharedPreferencesHelper = remember { SharedPreferencesHelper(context) }
     val selectedPlayers = remember { mutableStateListOf<PlayerData>() }
     val competitionName = sharedPreferencesHelper.competitionName
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.image_loading))
+
 
     LaunchedEffect(true) {
         viewModel.getPlayersByCompetitionType(competitionName.toString())
@@ -99,23 +106,21 @@ fun SelectPlayerScreen(
     }
 
     if (teamBalancerUIState.isLoading) {
-        Log.d("SelectPlayerScreenUI", "${teamBalancerUIState}")
-        Log.d("SelectPlayerScreenUI", "${teamBalancerUIState.isLoading}")
         Column(
             modifier = Modifier
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Text(text = "Creating teams...", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+            Text(
+                text = "Creating teams...",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
                 style = TextStyle(
                     color = APPBAR_GREEN,
-                    fontFamily = FontFamily(
-                        Font(
-                            R.font.onboarding_title1
-                        )
-                    )
-                )  )
+                    fontFamily = FontFamily(Font(R.font.onboarding_title1))
+                )
+            )
         }
     } else {
         Column(Modifier.fillMaxSize()) {
@@ -129,6 +134,8 @@ fun SelectPlayerScreen(
                     val player = playerListUiState.playerList!![index]
                     val isSelected = player in selectedPlayers
 
+                    var isFlipped by remember { mutableStateOf(false) }
+
                     Card(
                         modifier = Modifier
                             .background(Color.Transparent)
@@ -138,82 +145,49 @@ fun SelectPlayerScreen(
                                 color = if (isSelected) APPBAR_GREEN else Color.Transparent,
                                 shape = RoundedCornerShape(8.dp)
                             )
-                            .clickable {
-                                if (isSelected) {
-                                    selectedPlayers.remove(player)
-                                } else {
-                                    selectedPlayers.add(player)
-                                }
+                            .pointerInput(Unit) {
+                                detectTapGestures(
+                                    onPress = {
+                                        // Trigger selection on single tap
+                                        val pressStart = System.currentTimeMillis()
+                                        val result = tryAwaitRelease()
+                                        val pressEnd = System.currentTimeMillis()
+                                        val isLongPress = pressEnd - pressStart > 500 // Long press threshold
+
+                                        if (isLongPress) {
+                                            isFlipped = !isFlipped
+                                        } else {
+                                            if (isSelected) {
+                                                selectedPlayers.remove(player)
+                                            } else {
+                                                selectedPlayers.add(player)
+                                            }
+                                        }
+                                    }
+                                )
                             }
-                            .fillMaxWidth(),
+                            .fillMaxWidth()
                     ) {
                         Box(
                             modifier = Modifier.background(Color.White)
                         ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.player_card),
-                                contentDescription = null,
-                                modifier = Modifier.fillMaxSize(),
-                                contentScale = ContentScale.Crop
-                            )
-                            Column(
-                                modifier = Modifier
-                                    .padding(8.dp)
-                                    .align(Alignment.Center),
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                verticalArrangement = Arrangement.Center
+                            this@Card.AnimatedVisibility(
+                                visible = !isFlipped,
+                                enter = fadeIn(),
+                                exit = fadeOut(),
+                                modifier = Modifier.fillMaxSize()
                             ) {
-
-                                SubcomposeAsyncImage(
-                                    model = player.profilePhotoUrl,
-                                    contentDescription = null,
-                                    contentScale = ContentScale.Crop,
-                                    modifier = Modifier
-                                        .size(100.dp)
-                                        .clip(RoundedCornerShape(8.dp))
-                                        .border(1.dp, Color.Gray, RoundedCornerShape(8.dp)),
-                                    loading = {
-                                        LottieAnimation(
-                                            composition,
-                                            modifier = Modifier.size(100.dp),
-                                            iterations = Int.MAX_VALUE
-                                        )
-                                    }
-                                )
-                                Text(
-                                    text = "${player.firstName} ${player.lastName}",
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 18.sp,
-                                    color = Color.Black,
-                                    modifier = Modifier.padding(top = 4.dp)
-                                )
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(top = 4.dp),
-                                    horizontalArrangement = Arrangement.SpaceEvenly
-                                ) {
-                                    Column {
-                                        Text(text = "FOC: ${player.focus}", fontSize = 12.sp)
-                                        Text(text = "CON: ${player.condition}", fontSize = 12.sp)
-                                    }
-                                    Column {
-                                        Text(text = "SPE: ${player.speed}", fontSize = 12.sp)
-                                        Text(text = "DUR: ${player.durability}", fontSize = 12.sp)
-                                    }
-                                }
+                                FrontCardContent(player, { isFlipped = !isFlipped })
                             }
-                            Text(
-                                text = player.totalSkillRating.toString(),
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold,
-                                color = DARK_GREEN,
-                                modifier = Modifier
-                                    .align(Alignment.TopStart)
-                                    .padding(start = 20.dp, top = 40.dp)
-                                    .border(1.dp, Color.Black, RoundedCornerShape(4.dp))
-                                    .padding(4.dp)
-                            )
+
+                            this@Card.AnimatedVisibility(
+                                visible = isFlipped,
+                                enter = fadeIn(),
+                                exit = fadeOut(),
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                BackCardContent(player, { isFlipped = !isFlipped })
+                            }
                         }
                     }
                 }
@@ -262,3 +236,85 @@ fun SelectPlayerScreen(
     }
 }
 
+@Composable
+fun FrontCardContent(player: PlayerData, onFlip: () -> Unit) {
+    val compositionLottie by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.image_loading))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.player_card),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(12.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            SubcomposeAsyncImage(
+                model = player.profilePhotoUrl,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(1.dp, Color.Gray, RoundedCornerShape(8.dp)),
+                loading = {
+                    LottieAnimation(
+                        composition = compositionLottie,
+                        modifier = Modifier.size(100.dp),
+                        iterations = Int.MAX_VALUE
+                    )
+                }
+            )
+            Text(
+                text = "${player.firstName} ${player.lastName}",
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                color = Color.Black,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = "Rating: ${player.totalSkillRating}",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = DARK_GREEN,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
+
+@Composable
+fun BackCardContent(player: PlayerData, onFlip: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White.copy(alpha = 0.8f), RoundedCornerShape(12.dp))
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Speed: ${player.speed}", fontSize = 16.sp)
+            Text(text = "Focus: ${player.focus}", fontSize = 16.sp)
+            Text(text = "Condition: ${player.condition}", fontSize = 16.sp)
+            Text(text = "Durability: ${player.durability}", fontSize = 16.sp)
+        }
+    }
+}
