6e1b7008805edb66bc1a2c7d90f9eef8192d1612
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-06 01:51:51 +0300

    feat:modifier added as a parameter

--- feature/src/main/java/com/yusuf/component/auth_components/AuthButtonComponent.kt
+++ feature/src/main/java/com/yusuf/component/auth_components/AuthButtonComponent.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.yusuf.theme.APPBAR_GREEN
@@ -22,26 +23,33 @@ import com.yusuf.theme.DARK_GREEN
 
 
 @Composable
-fun AuthButtonComponent(value: String, onClick: () -> Unit) {
+fun AuthButtonComponent(
+    value: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    fillMaxWidth: Boolean = true,
+    heightIn: Dp = 48.dp
+) {
     Button(
         onClick = { onClick() },
-        modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(48.dp),
+        modifier = modifier
+            .then(if (fillMaxWidth) Modifier.fillMaxWidth() else Modifier)
+            .heightIn(heightIn),
         contentPadding = PaddingValues(),
         colors = ButtonDefaults.buttonColors(Color.Red)
     ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .heightIn(48.dp)
+                .heightIn(heightIn)
                 .background(
                     brush = Brush.verticalGradient(
                         listOf(
                             APPBAR_GREEN,
                             DARK_GREEN
                         )
-                    ), shape = RoundedCornerShape(50.dp)
+                    ),
+                    shape = RoundedCornerShape(50.dp)
                 ),
             contentAlignment = Alignment.Center
         ) {
