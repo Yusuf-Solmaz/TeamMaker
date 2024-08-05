9575498114561f212b2d55d94bfbdef3e80bc081
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-08-05 11:45:05 +0300

    feat: used the calculateTeamAverageSkillRatingUseCase in the TeamBalancerViewModel

--- feature/src/main/java/com/yusuf/feature/create_competition/select_player/viewmodel/TeamBalancerViewModel.kt
+++ feature/src/main/java/com/yusuf/feature/create_competition/select_player/viewmodel/TeamBalancerViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yusuf.domain.model.firebase.PlayerData
+import com.yusuf.domain.use_cases.team.CalculateTeamAverageSkillRatingUseCase
 import com.yusuf.domain.use_cases.team.TeamBalancerUseCase
 import com.yusuf.domain.util.RootResult
 import com.yusuf.feature.create_competition.select_player.state.TeamBalancerUIState
@@ -16,7 +17,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TeamBalancerViewModel @Inject constructor(
-    private val teamBalancerUseCase: TeamBalancerUseCase
+    private val teamBalancerUseCase: TeamBalancerUseCase,
+    private val calculateTeamAverageSkillRatingUseCase: CalculateTeamAverageSkillRatingUseCase
 ) : ViewModel() {
     private val _teamBalancerUiState = MutableStateFlow(TeamBalancerUIState())
     val teamBalancerUiState: StateFlow<TeamBalancerUIState> get() = _teamBalancerUiState
@@ -28,26 +30,42 @@ class TeamBalancerViewModel @Inject constructor(
             teamBalancerUseCase(players).collect { result ->
                 when (result) {
                     is RootResult.Success -> {
-                        _teamBalancerUiState.value = _teamBalancerUiState.value.copy(
-                            teams = result.data,
-                            isLoading = false,
-                            errorMessage = null
-                        )
-                        Log.d("SharedViewModel", "Teams set: ${_teamBalancerUiState.value.teams}")
+                        result.data?.let { (teamFirst, teamSecond) ->
+                            val teamFirstAverage = calculateTeamAverageSkillRatingUseCase(teamFirst)
+                            val teamSecondAverage = calculateTeamAverageSkillRatingUseCase(teamSecond)
+
+                            _teamBalancerUiState.value = _teamBalancerUiState.value.copy(
+                                teams = result.data,
+                                teamAverageSkillRating = teamFirstAverage to teamSecondAverage,
+                                isLoading = false,
+                                errorMessage = null
+                            )
+                            Log.d("TeamBalancerViewModel", "Teams set: ${_teamBalancerUiState.value.teams}")
+                            Log.d("TeamBalancerViewModel", "Team Average Skill Ratings: $teamFirstAverage and $teamSecondAverage")
+                        } ?: run {
+                            _teamBalancerUiState.value = _teamBalancerUiState.value.copy(
+                                teams = null,
+                                teamAverageSkillRating = null,
+                                isLoading = false,
+                                errorMessage = "Teams data is null"
+                            )
+                            Log.e("TeamBalancerViewModel", "Teams data is null")
+                        }
                     }
                     is RootResult.Error -> {
                         _teamBalancerUiState.value = _teamBalancerUiState.value.copy(
                             teams = null,
+                            teamAverageSkillRating = null,
                             isLoading = false,
                             errorMessage = result.message
                         )
-                        Log.e("SharedViewModel", "Error creating balanced teams: ${result.message}")
+                        Log.e("TeamBalancerViewModel", "Error creating balanced teams: ${result.message}")
                     }
                     is RootResult.Loading -> {
                         _teamBalancerUiState.value = _teamBalancerUiState.value.copy(
                             isLoading = true
                         )
-                        Log.d("SharedViewModel", "Loading creating balanced teams...")
+                        Log.d("TeamBalancerViewModel", "Loading creating balanced teams...")
                     }
                 }
             }
