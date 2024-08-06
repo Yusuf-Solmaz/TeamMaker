77c731d34071408070d0b9699013e5f56bc69c2a
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-06 15:49:57 +0300

    refactor:timepicker moved to another file

---
+++ feature/src/main/java/com/yusuf/feature/create_competition/TimePicker.kt
@@ -0,0 +1,177 @@
+package com.yusuf.feature.create_competition
+
+import android.icu.text.SimpleDateFormat
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.yusuf.feature.R
+import java.util.Calendar
+import java.util.Locale
+
+@Composable
+fun TimePicker(onTimeSelected: (String) -> Unit) {
+    var showAdvancedExample by remember { mutableStateOf(false) }
+    var selectedTime: String? by remember { mutableStateOf(null) }
+    val formatter = remember { SimpleDateFormat("hh:mm a", Locale.getDefault()) }
+
+    Column(
+        modifier = Modifier
+            .width(150.dp)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        IconButton(onClick = {
+            showAdvancedExample = true
+        }) {
+            Text("")
+            Icon(painterResource(R.drawable.baseline_watch_later_24), contentDescription = null)
+        }
+        if (selectedTime != null) {
+            Text("Selected time = $selectedTime")
+        } else {
+            Text("No time selected.")
+        }
+    }
+
+    when {
+        showAdvancedExample -> AdvancedTimePickerExample(
+            onDismiss = { showAdvancedExample = false },
+            onConfirm = { time ->
+                selectedTime = time
+                onTimeSelected(time)
+                showAdvancedExample = false
+            },
+        )
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AdvancedTimePickerExample(
+    onConfirm: (String) -> Unit, // We'r using string instead of timestate
+    onDismiss: () -> Unit,
+) {
+
+    val currentTime = Calendar.getInstance()
+
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = true,
+    )
+
+    var showDial by remember { mutableStateOf(true) }
+
+    val toggleIcon = if (showDial) {
+        Icons.Filled.Add
+    } else {
+        Icons.Filled.Build
+    }
+
+    AdvancedTimePickerDialog(
+        onDismiss = { onDismiss() },
+        onConfirm = {
+            val selectedTime = "${timePickerState.hour}:${timePickerState.minute}"
+            onConfirm(selectedTime)
+        },
+        toggle = {
+            IconButton(onClick = { showDial = !showDial }) {
+                Icon(
+                    imageVector = toggleIcon,
+                    contentDescription = "Time picker type toggle",
+                )
+            }
+        },
+    ) {
+        if (showDial) {
+            androidx.compose.material3.TimePicker(state = timePickerState)
+        } else {
+            TimeInput(state = timePickerState)
+        }
+    }
+}
+
+
+@Composable
+fun AdvancedTimePickerDialog(
+    title: String = "Select Time",
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    toggle: @Composable () -> Unit = {},
+    content: @Composable () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
+            modifier =
+            Modifier
+                .width(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = MaterialTheme.colorScheme.surface
+                ),
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                content()
+                Row(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    toggle()
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(onClick = onDismiss) { Text("Cancel") }
+                    TextButton(onClick = onConfirm) { Text("OK") }
+                }
+            }
+        }
+    }
+}
