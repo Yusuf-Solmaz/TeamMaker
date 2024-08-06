86a6618541246e9a19524b4b1eae9530ca5107f3
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-06 13:34:39 +0300

    feat:button design changed

--- feature/src/main/java/com/yusuf/feature/options/OptionsScreen.kt
+++ feature/src/main/java/com/yusuf/feature/options/OptionsScreen.kt
@@ -4,16 +4,22 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +30,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.yusuf.component.auth_components.AuthButtonComponent
 import com.yusuf.feature.R
 import com.yusuf.navigation.NavigationGraph
 import com.yusuf.theme.APPBAR_GREEN
@@ -34,7 +41,7 @@ import com.yusuf.utils.default_competition.Competition
 fun OptionsScreen(
     navController: NavController,
     competition: Competition
-    ) {
+) {
 
 
     Log.d("OptionsScreen", "Competition: $competition")
@@ -66,7 +73,7 @@ fun OptionsScreen(
 
 @Composable
 fun OptionsCard(
-    text:String,
+    text: String,
     navController: NavController,
     imageResourceId: Int,
     route: String
@@ -76,39 +83,39 @@ fun OptionsCard(
             .fillMaxWidth()
             .padding(8.dp)
             .clickable { navController.navigate(route) },
-        elevation = CardDefaults.cardElevation(4.dp)
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
     ) {
-
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
+                .height(240.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
             Image(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(170.dp),
                 painter = painterResource(id = imageResourceId),
                 contentDescription = null,
-                contentScale = ContentScale.FillBounds,
+                contentScale = ContentScale.FillBounds
             )
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            Modifier.fillMaxWidth(),
-
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically){
-            Button(
-                onClick = {
-                    navController.navigate(route)
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = APPBAR_GREEN,
-                    contentColor = Color.White
+            Spacer(modifier = Modifier.height(8.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                AuthButtonComponent(
+                    onClick = { navController.navigate(route) },
+                    value = text,
+                    modifier = Modifier
+                        .heightIn(24.dp)
+                        .width(120.dp)
                 )
-            ){
-                Text(text = text)
             }
         }
-
     }
 }
