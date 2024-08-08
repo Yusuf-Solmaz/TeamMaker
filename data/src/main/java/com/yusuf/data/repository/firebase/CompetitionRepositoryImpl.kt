0d7619663e18dab1cfc43976d4f5073aa01485f7
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-08-08 16:21:00 +0300

    feat: added repo impl fun and usecases for delete and get saved competitions

--- data/src/main/java/com/yusuf/data/repository/firebase/CompetitionRepositoryImpl.kt
+++ data/src/main/java/com/yusuf/data/repository/firebase/CompetitionRepositoryImpl.kt
@@ -3,7 +3,9 @@ package com.yusuf.data.repository.firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.yusuf.data.mapper.firebase_mapper.toSavedCompetitions
 import com.yusuf.data.mapper.firebase_mapper.toSavedCompetitionsDto
+import com.yusuf.data.remote.dto.firebase_dto.SavedCompetitionsDto
 import com.yusuf.domain.model.firebase.SavedCompetitionsModel
 import com.yusuf.domain.repository.firebase.competition.CompetitionRepository
 import com.yusuf.domain.util.RootResult
@@ -42,12 +44,51 @@ class CompetitionRepositoryImpl @Inject constructor(
         }
     }.flowOn(Dispatchers.IO)
 
-    override suspend fun getAllSavedCompetitions(): Flow<RootResult<List<SavedCompetitionsModel>>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getAllSavedCompetitions(): Flow<RootResult<List<SavedCompetitionsModel>>> = flow {
+        emit(RootResult.Loading)
+        try {
+            val currentUser = firebaseAuth.currentUser
+            val userId = currentUser?.uid
+            if (userId != null) {
+                val querySnapshot = firestore.collection("users").document(userId)
+                    .collection("savedCompetitions").get().await()
+                val savedCompetitionList = querySnapshot.documents.mapNotNull { document ->
+                    val savedCompetitionListDto =
+                        document.toObject(SavedCompetitionsDto::class.java)
+                    savedCompetitionListDto?.toSavedCompetitions()
+                }
+                emit(RootResult.Success(savedCompetitionList))
+            }else{
+                emit(RootResult.Error("User not found"))
+            }
+        }catch (e:Exception){
+            emit(RootResult.Error(e.message ?: "Something went wrong"))
+        }
+    }.flowOn(Dispatchers.IO)
 
-    override suspend fun deleteSavedCompetition(competitionId: String): Flow<RootResult<Boolean>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteSavedCompetition(competitionId: String): Flow<RootResult<Boolean>> = flow {
+        emit(RootResult.Loading)
+        try {
+            val currentUser = firebaseAuth.currentUser
+            val userId = currentUser?.uid
+            if (userId != null) {
+                val competitionCollection = firestore.collection("users").document(userId).collection("savedCompetitions")
+                val querySnapshot = competitionCollection.whereEqualTo("competitionId", competitionId).get().await()
+                val competitionDocument = querySnapshot.documents
 
+                if (competitionDocument.isNotEmpty()) {
+                    competitionDocument.forEach { document ->
+                        document.reference.delete().await()
+                    }
+                    emit(RootResult.Success(true))
+                } else {
+                    emit(RootResult.Error("Competition not found"))
+                }
+            } else {
+                emit(RootResult.Error("User not found"))
+            }
+        } catch (e: Exception) {
+            emit(RootResult.Error(e.message ?: "Something went wrong"))
+        }
+    }.flowOn(Dispatchers.IO)
 }
