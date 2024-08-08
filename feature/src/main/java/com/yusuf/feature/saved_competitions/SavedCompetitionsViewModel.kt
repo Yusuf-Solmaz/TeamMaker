4b12d3b950d93dd342abf1ee9e8b8d58a5cf68cb
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-08-08 16:41:46 +0300

    feat: added uiStates, getSavedCompetitions and deleteSavedCompetitionand

---
+++ feature/src/main/java/com/yusuf/feature/saved_competitions/SavedCompetitionsViewModel.kt
@@ -0,0 +1,100 @@
+package com.yusuf.feature.saved_competitions
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yusuf.domain.use_cases.firebase_use_cases.competition.DeleteSavedCompetitionUseCase
+import com.yusuf.domain.use_cases.firebase_use_cases.competition.GetAllSavedCompetitionsUseCase
+import com.yusuf.domain.util.RootResult
+import com.yusuf.feature.saved_competitions.state.DeleteSavedCompetitionsUIState
+import com.yusuf.feature.saved_competitions.state.GetSavedCompetitionsUIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SavedCompetitionsViewModel @Inject constructor(
+    private val getAllSavedCompetitionsUseCase: GetAllSavedCompetitionsUseCase,
+    private val deleteSavedCompetitionUseCase: DeleteSavedCompetitionUseCase
+) : ViewModel() {
+
+    private val _getSavedCompetitionsUIState = MutableStateFlow(GetSavedCompetitionsUIState())
+    val getSavedCompetitionsUIState : StateFlow<GetSavedCompetitionsUIState> = _getSavedCompetitionsUIState
+
+    private val _deleteSavedCompetitionUIState = MutableStateFlow(DeleteSavedCompetitionsUIState())
+    val deleteSavedCompetitionUIState : StateFlow<DeleteSavedCompetitionsUIState> = _deleteSavedCompetitionUIState
+
+    fun getSavedCompetitions() {
+        _getSavedCompetitionsUIState.value =
+            _getSavedCompetitionsUIState.value.copy(isLoading = true)
+        viewModelScope.launch {
+            getAllSavedCompetitionsUseCase().collect { result ->
+                when (result) {
+                    is RootResult.Error -> {
+                        _getSavedCompetitionsUIState.value =
+                            _getSavedCompetitionsUIState.value.copy(
+                                error = result.message,
+                                isLoading = false,
+                                savedCompetitions = emptyList()
+                            )
+                    }
+
+                    is RootResult.Loading -> {
+                        _getSavedCompetitionsUIState.value =
+                            _getSavedCompetitionsUIState.value.copy(
+                                isLoading = true,
+                                error = null,
+                                savedCompetitions = emptyList()
+                            )
+                    }
+
+                    is RootResult.Success -> {
+                        _getSavedCompetitionsUIState.value =
+                            _getSavedCompetitionsUIState.value.copy(
+                                savedCompetitions = result.data,
+                                isLoading = false,
+                                error = null
+                            )
+                    }
+                }
+            }
+        }
+    }
+
+    fun deleteSavedCompetition(competitionId: String) {
+        _deleteSavedCompetitionUIState.value =
+            _deleteSavedCompetitionUIState.value.copy(isLoading = true)
+        viewModelScope.launch {
+            deleteSavedCompetitionUseCase(competitionId).collect { result ->
+                when (result) {
+                    is RootResult.Loading -> {
+                        _deleteSavedCompetitionUIState.value =
+                            _deleteSavedCompetitionUIState.value.copy(
+                                isLoading = true,
+                                error = null,
+                                transaction = false
+                            )
+                    }
+                    is RootResult.Error -> {
+                        _deleteSavedCompetitionUIState.value =
+                            _deleteSavedCompetitionUIState.value.copy(
+                                error = result.message,
+                                isLoading = false,
+                                transaction = false
+                            )
+                    }
+                    is RootResult.Success -> {
+                        _deleteSavedCompetitionUIState.value =
+                            _deleteSavedCompetitionUIState.value.copy(
+                                isLoading = false,
+                                error = null,
+                                transaction = true
+                            )
+                        getSavedCompetitions()
+                    }
+                }
+            }
+        }
+    }
+}
