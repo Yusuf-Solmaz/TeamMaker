9729be9f14f061c89994b718c7d03e36a8fa3ecd
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-09 20:07:24 +0300

    feat:chose image added into icon

--- feature/src/main/java/com/yusuf/feature/create_competition/ImagePicker.kt
+++ feature/src/main/java/com/yusuf/feature/create_competition/ImagePicker.kt
@@ -4,14 +4,18 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,30 +34,42 @@ fun ImagePickerComposable(
             onImageSelected(uri)
         }
     )
-
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxWidth()
     ) {
-        IconButton(onClick = { launcher.launch("image/*") }) {
-            Icon(Icons.Default.Add, contentDescription = "Select Image")
-        }
-
-        imageUri.value?.let { uri ->
-            Image(
-                painter = rememberAsyncImagePainter(model = uri),
-                contentDescription = "Selected Image",
-                modifier = Modifier
-                    .size(100.dp)
-                    .padding(start = 16.dp),
-                contentScale = ContentScale.Crop
-            )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+                .background(color = Color.Gray.copy(alpha = 0.3f))
+                .clickable { launcher.launch("image/*") }
+        ) {
+            if (imageUri.value != null) {
+                Image(
+                    painter = rememberAsyncImagePainter(model = imageUri.value),
+                    contentDescription = "Selected Image",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Select Image",
+                    modifier = Modifier.size(50.dp),
+                    tint = Color.White
+                )
+            }
         }
     }
 }
-@Preview
-@Composable
-fun ImagePickerComposablePreview() {
-    ImagePickerComposable(onImageSelected = {})
-}
+
+    @Preview
+    @Composable
+    fun ImagePickerComposablePreview() {
+        ImagePickerComposable(onImageSelected = {})
+    }
