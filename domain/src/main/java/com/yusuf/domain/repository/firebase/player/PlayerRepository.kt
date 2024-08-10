62c0e7da663d9e9f2e8bfb65dcc67a12a7454d07
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-08-10 14:19:13 +0300

    feat: add image method update

--- domain/src/main/java/com/yusuf/domain/repository/firebase/player/PlayerRepository.kt
+++ domain/src/main/java/com/yusuf/domain/repository/firebase/player/PlayerRepository.kt
@@ -8,18 +8,17 @@ import kotlinx.coroutines.flow.Flow
 
 interface PlayerRepository {
     suspend fun getAllPlayers(): Flow<RootResult<List<PlayerData>>>
-    suspend fun addPlayer(playerData: PlayerData,  imageUri: Uri): Flow<RootResult<Boolean>>
+    suspend fun addPlayer(playerData: PlayerData,  imageUri: Uri, imagePathString: String): Flow<RootResult<Boolean>>
     suspend fun addCompetition(competitionData: CompetitionData): Flow<RootResult<Boolean>>
     suspend fun deleteCompetition(competitionId: String): Flow <RootResult<Boolean>>
     suspend fun getAllCompetitions(): Flow<RootResult<List<CompetitionData>>>
     fun getCurrentUserId(): Flow<RootResult<String?>>
     suspend fun deletePlayerById(playerId: String): Flow<RootResult<Boolean>>
     suspend fun updatePlayerById(playerId: String, updatedPlayerData: PlayerData) : Flow<RootResult<Boolean>>
-    suspend fun uploadImage(uri: Uri): Flow<RootResult<String>>
+    suspend fun uploadImage(uri: Uri,imagePathString: String): Flow<RootResult<String>>
     suspend fun updatePlayerImage(uri: Uri, onSuccess: (String) -> Unit, onFailure: (Exception) -> Unit): Flow<RootResult<Boolean>>
     suspend fun getPlayersByCompetitionType(competitionType: String): Flow<RootResult<List<PlayerData>>>
     suspend fun updateCompetition(competitionId: String, competitionData: CompetitionData): Flow<RootResult<Boolean>>
     suspend fun deleteCurrentUser(): Flow<RootResult<Boolean>>
-    suspend fun uploadImageCompetition(uri: Uri): Result<String>
 }
 
