cf81e2c1ec47be4d2a0578c500b96eb336308622
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-08-11 23:44:53 +0300

    feat: card used in the screen

--- feature/src/main/java/com/yusuf/feature/saved_competitions/SavedCompetitionsScreen.kt
+++ feature/src/main/java/com/yusuf/feature/saved_competitions/SavedCompetitionsScreen.kt
@@ -1,53 +1,24 @@
 package com.yusuf.feature.saved_competitions
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
 import com.yusuf.component.LoadingLottie
-import com.yusuf.component.auth_components.AuthButtonComponent
-import com.yusuf.domain.model.firebase.SavedCompetitionsModel
 import com.yusuf.feature.R
 import com.yusuf.feature.saved_competitions.state.GetSavedCompetitionsUIState
-import com.yusuf.navigation.NavigationGraph
-import com.yusuf.theme.DARK_RED
-import com.yusuf.theme.RED
+import com.yusuf.utils.SharedPreferencesHelper
 
 @Composable
 fun SavedCompetitionsScreen(
@@ -55,7 +26,9 @@ fun SavedCompetitionsScreen(
     viewModel: SavedCompetitionsViewModel = hiltViewModel()
 ) {
     val state by viewModel.getSavedCompetitionsUIState.collectAsState(GetSavedCompetitionsUIState())
-
+    val context= LocalContext.current
+    val sharedPreferencesHelper = SharedPreferencesHelper(context)
+    val competitionName = sharedPreferencesHelper.competitionName
 
     Column(
         modifier = Modifier
