9d830ec71f379f56b8383b7443bde5fcbca98fb3
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-08-12 20:58:06 +0300

    feat: updated weatherLottieAnm file according to main name

--- feature/src/main/java/com/yusuf/utils/WeatherLottieAnim.kt
+++ feature/src/main/java/com/yusuf/utils/WeatherLottieAnim.kt
@@ -4,16 +4,14 @@ import com.yusuf.domain.model.weather.WeatherModel
 import com.yusuf.feature.R
 
 fun WeatherModel.getLottieAnimationResource() : Int{
-    return when(this.description){
-        "clear sky" -> R.raw.clear_sky_anim
-        "few clouds" -> R.raw.few_clouds_anim
-        "scattered clouds" -> R.raw.few_clouds_anim
-        "broken clouds" -> R.raw.broken_clouds_anim
-        "shower rain" -> R.raw.shower_rain_anim
-        "rain" -> R.raw.rainy_anim
-        "thunderstorm" -> R.raw.thunderstorm_anim
-        "snow" -> R.raw.snow_anim
-        "mist" -> R.raw.mist_anim
+    return when(this.main){
+        "Clear" -> R.raw.clear_sky_anim
+        "Clouds" -> R.raw.few_clouds_anim
+        "Rain" -> R.raw.shower_rain_anim
+        "Drizzle" -> R.raw.rainy_anim
+        "Thunderstorm" -> R.raw.thunderstorm_anim
+        "Snow" -> R.raw.snow_anim
+        "Atmosphere" -> R.raw.mist_anim
         else -> R.raw.few_clouds_anim
     }
 }
